c47ad7f47a91c7f19394a9e95b40f4c9182a57a4
Lucas McClean <lucksdeveloper@gmail.com>
2024-06-24 18:05:23 +0200

    feat: add insertion sort

---
+++ algs/isort/insertionsort.go
@@ -0,0 +1,15 @@
+package isort
+
+import u "github.com/ljmcclean/dsa-go/utils"
+
+func Sort[O u.Ordered](arr []O) {
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		j := i - 1
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
+		}
+		arr[j+1] = key
+	}
+}
