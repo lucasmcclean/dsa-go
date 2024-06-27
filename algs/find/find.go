a531a969f5943b13bf61edc84f398e9851fa9712
Lucas McClean <lucksdeveloper@gmail.com>
2024-06-27 16:11:53 +0200

    style: rename 'binsearch' (binary search) to 'find' for brevity

--- algs/find/find.go
+++ algs/find/find.go
@@ -1,8 +1,10 @@
-package binsearch
+package find
 
 import u "github.com/ljmcclean/dsa-go/utils"
 
-func Search[O u.Ordered](arr []O, target O) (index int) {
+// Find uses binary search to locate an element in a sorted slice
+// of ordered elements.
+func Find[O u.Ordered](arr []O, target O) (index int) {
 	l, r := 0, len(arr)-1
 	for l <= r {
 		mid := (r-l)/2 + l
--- algs/find/find_test.go
+++ algs/find/find_test.go
@@ -1,4 +1,4 @@
-package binsearch
+package find
 
 import (
 	"testing"
@@ -7,13 +7,13 @@ import (
 func TestSearchInts(t *testing.T) {
 	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	got := Search(slice, 3)
+	got := Find(slice, 3)
 	want := 2
 	if got != want {
 		t.Errorf("Got %d, wanted %d", got, want)
 	}
 
-	got = Search(slice, -5)
+	got = Find(slice, -5)
 	want = -1
 	if got != want {
 		t.Errorf("Got %d, wanted %d", got, want)
@@ -23,13 +23,13 @@ func TestSearchInts(t *testing.T) {
 func TestSearchNegatives(t *testing.T) {
 	array := [10]int{-10, -5, -3, -1, 0, 1, 3, 5, 10}
 
-	got := Search(array[:], -3)
+	got := Find(array[:], -3)
 	want := 2
 	if got != want {
 		t.Errorf("Got %d, wanted %d", got, want)
 	}
 
-	got = Search(array[:], -100)
+	got = Find(array[:], -100)
 	want = -1
 	if got != want {
 		t.Errorf("Got %d, wanted %d", got, want)
@@ -39,7 +39,7 @@ func TestSearchNegatives(t *testing.T) {
 func TestSearchStrings(t *testing.T) {
 	slice := []string{"A", "B", "C", "D", "E", "F", "G"}
 
-	got := Search(slice, "B")
+	got := Find(slice, "B")
 	want := 1
 	if got != want {
 		t.Errorf("Got %d, wanted %d", got, want)
@@ -48,7 +48,7 @@ func TestSearchStrings(t *testing.T) {
 
 func TestSearchNil(t *testing.T) {
 	var slice []int
-	_ = Search(slice, 1)
+	_ = Find(slice, 1)
 	slice = make([]int, 10)
-	_ = Search(slice, 1)
+	_ = Find(slice, 1)
 }
