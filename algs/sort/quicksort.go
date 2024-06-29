608e4a7c48096094744c4e92b54a43a4d838168e
Lucas McClean <lucksdeveloper@gmail.com>
2024-06-29 16:58:21 +0200

    feat: add quicksort variation for mostly unique elements
    unique quicksort uses Hoare partitioning rather than three-way to optimize in the case of primarily unique elements, remove parallel quicksort as it is unnecessary

--- algs/sort/quicksort.go
+++ algs/sort/quicksort.go
@@ -1,29 +1,43 @@
 package sort
 
-import "github.com/ljmcclean/dsa-go/types"
+import (
+	"github.com/ljmcclean/dsa-go/types"
+)
 
+const insertionCutoff = 25
+
+// Optimized for case of duplicate elements.
 func QuickSort[O types.Ordered](arr []O) {
-	if arr == nil {
-		return
-	} else if len(arr) == 1 {
-		return
+	for {
+		if len(arr) < insertionCutoff {
+			InsertionSort(arr)
+			return
+		}
+		piv := arr[ninther(arr)]
+		l, r := partition(arr, piv)
+		QuickSort(arr[:l])
+		arr = arr[r:]
 	}
-	UQuickSort(arr)
 }
 
-// Bypasses the check made by Sort to ensure arr isn't nil
-// and that the arr is longer than one element.
-func UQuickSort[O types.Ordered](arr []O) {
-	for len(arr) > 0 {
-		pil, pir := partition(arr)
-		UQuickSort(arr[:pil])
-		arr = arr[pir:]
+// Optimized for case of mostly unique elements.
+func QuickSortUnique[O types.Ordered](arr []O) {
+	if len(arr) < insertionCutoff {
+		InsertionSort(arr)
+		return
+	}
+	piv := arr[ninther(arr)]
+	l, r := hoarePartition(arr, piv)
+	if r > 0 {
+		QuickSortUnique(arr[:r+1])
+	}
+	if l < len(arr)-1 {
+		QuickSortUnique(arr[l:])
 	}
 }
 
 // Three-way partition for case of duplicate elements.
-func partition[O types.Ordered](arr []O) (lPivIdx, rPivIdx int) {
-	piv := ninther(arr)
+func partition[O types.Ordered](arr []O, piv O) (lPivIdx, rPivIdx int) {
 	l, r := 0, len(arr)
 	for i := 0; i < r; {
 		switch {
@@ -41,14 +55,33 @@ func partition[O types.Ordered](arr []O) (lPivIdx, rPivIdx int) {
 	return l, r
 }
 
+// Hoare partition for case of mostly unique elements.
+func hoarePartition[O types.Ordered](arr []O, piv O) (lPivIdx, rPivIdx int) {
+	l, r := 0, len(arr)-1
+	for l <= r {
+		for arr[l] < piv {
+			l++
+		}
+		for arr[r] > piv {
+			r--
+		}
+		if l <= r {
+			arr[l], arr[r] = arr[r], arr[l]
+			l++
+			r--
+		}
+	}
+	return l, r
+}
+
 // Implementation of Tukey's ninther (median of medians) to
 // ensure more consistent performance.
-func ninther[O types.Ordered](arr []O) (piv O) {
+func ninther[O types.Ordered](arr []O) (pivIdx int) {
 	n := len(arr)
 	a := medOfThree(arr, 0, n/6, n/3)
 	b := medOfThree(arr, n/3, 5*n/6, 2*n/3)
 	c := medOfThree(arr, 2*n/3, 5*n/6, n-1)
-	return arr[medOfThree(arr, a, b, c)]
+	return medOfThree(arr, a, b, c)
 }
 
 // Find the median value of arr between three indexes and return
